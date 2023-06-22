59d5d03131e38e999b2c09d27d83c867930435d7
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-06-23 00:22:42 +0200

    .

--- FrontEnd/assets/script.js
+++
@@ -1,7 +0,0 @@
-
-
-let page = await fetch("http://localhost:5678/api/works");
-let categories = await fetch('http://localhost:5678/api/categories');
-
-const PageReponse = await page.json();
-const CategoriesReponse = await categories.json();
