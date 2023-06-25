69b2daed3cbafd5e5d034b66df5e02895c4ee484
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-06-25 03:20:39 +0200

    test

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -20,10 +20,13 @@ bouton.forEach((button) => {
 
 
 // Partie login
+
 const login = {
-  email: 'string',
-  password: 'string'
-};
+  email: 'sophie.bluel@test.tld',
+  password: 's0phie',
+  }
+
+
 const body = JSON.stringify(login);
 
 
@@ -35,9 +38,31 @@ fetch('http://localhost:5678/api/users/login', {
   body: body
 })
 
+
+
 .then(() => console.log("ok"))
-.catch();
 
 
-
-// Partie
+const postLogin = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non Autorisé !' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: 1,
+            token: jwt.sign(
+              { userId: 1 },
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4',
+            )
+          });
+        })
+        .catch(error => res.status(404).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
