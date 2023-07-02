a37a241667e3aeaeb738264b7250b522a2b0593e
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-07-03 00:31:31 +0200

    .

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,29 +1,42 @@
- let bouton = document.querySelectorAll('.project button');
-let images = document.querySelectorAll('.gallery figure');
 let url = 'http://localhost:5678/api/categories';
 let gallery = document.querySelector("gallery");
 let figure = document.createElement('figure');
 let img = document.createElement('img');
 let figcaption = document.createElement('figcaption');
 
-
-
-let filtrerParCategorie = (url) => {
-
-  images.forEach((image) => {
-    let imageId = image.querySelector('img').id;
-    image.style.display = imageId === category || category === '' ? 'block' : 'none';
-  });
-};
-
-
-bouton.forEach((button) => {
-  button.addEventListener('click', () => {
-    let url = button.id; 
-    filtrerParCategorie(category);
-  });
-});
-
-
 // Partie login
 
+let login = document.getElementById('login');
+
+let Token = sessionStorage.getItem('accessToken');
+
+if (Token) {
+    login.innerText = 'logout';
+
+    login.addEventListener('click', logout);
+} else {
+    login.addEventListener('click', performLogin);
+}
+
+function performLogin() {
+    let email = document.getElementById('email').value;
+    let pass = document.getElementById('password').value;
+
+    if (email === 'sophie.bluel@test.tld' && pass === 'test123') {
+        // Remplacez le code suivant par la logique de connexion réelle, par exemple, une requête à votre API pour vérifier les informations d'identification
+        let userId = 1;
+        let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4';
+
+        sessionStorage.setItem('accessToken', token);
+        sessionStorage.setItem('userId', userId);
+        window.location.href = 'FrontEnd/index.html';
+    } else {
+        alert('Email ou mot de passe incorrects !');
+    }
+}
+
+function logout() {
+    sessionStorage.removeItem('accessToken');
+    sessionStorage.removeItem('userId');
+    window.location.reload();
+}
