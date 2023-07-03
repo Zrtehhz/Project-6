807135c17faff51e8042b50849c47be920d3f9dd
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-07-03 11:58:45 +0200

    .

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,77 +1,33 @@
-let url = 'http://localhost:5678/api/categories';
-let figure = document.createElement('figure');
-let img = document.createElement('img');
-let figcaption = document.createElement('figcaption');
-let gallery = document.querySelector("gallery");
+let bouton = document.querySelectorAll('.project button');
+let images = document.querySelectorAll('.gallery figure');
 
-
-const filterBtns = document.querySelectorAll('button')
+const url = 'http://localhost:5678/api/categories';
 
 const fetchData = async () => {
   try {
-    const response = await fetch(url)
-    const data = await response.json()
-    if (response.status === 200) {
-      filterBtns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-          let id = userId.id
-          //   console.log(data)
-          data.filter((league) => {
-            let res = league.competition
-            if (res.includes(id)) {
-              return res
-            }
-          })
-        })
-      })
-      let output = data.map((result) => {
-        return ''
-      })
-      output = output.join(' ')
-      content.innerHTML = output
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      bouton.forEach((button) => {
+        button.addEventListener('click', () => {
+          let category = button.id;
+          filtrerParCategorie(category, data);
+        });
+      });
+    } else {
+      throw new Error('Erreur lors de la récupération des données');
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-fetchData()
+const filtrerParCategorie = (category, data) => {
+  images.forEach((image) => {
+    let imageId = image.querySelector('img').id;
+    let match = data.find((item) => item.id.toString() === imageId);
+    image.style.display = match && match.category === category || category === 'tous' ? 'block' : 'none';
+  });
+};
 
-
-
-// Partie login
-
-let login = document.getElementById('login');
-
-let Token = sessionStorage.getItem('accessToken');
-
-if (Token) {
-    login.innerText = 'logout';
-
-    login.addEventListener('click', logout);
-} else {
-    login.addEventListener('click', performLogin);
-}
-
-function performLogin() {
-    let email = document.getElementById('email').value;
-    let pass = document.getElementById('password').value;
-
-    if (email === 'sophie.bluel@test.tld' && pass === 'test123') {
-        // Remplacez le code suivant par la logique de connexion réelle, par exemple, une requête à votre API pour vérifier les informations d'identification
-        let userId = 1;
-        let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4';
-
-        sessionStorage.setItem('accessToken', token);
-        sessionStorage.setItem('userId', userId);
-        window.location.href = 'FrontEnd/index.html';
-    } else {
-        alert('Email ou mot de passe incorrects !');
-    }
-}
-
-function logout() {
-    sessionStorage.removeItem('accessToken');
-    sessionStorage.removeItem('userId');
-    window.location.reload();
-}
+fetchData().catch(console.error);
