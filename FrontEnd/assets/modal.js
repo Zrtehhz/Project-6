c6da6f67084816760701ba2ace5e8d191f5827cd
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-07-27 03:43:45 +0200

    .

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -25,6 +25,88 @@ modalOverlay.addEventListener('click', (event) => {
 });
 
 
+// Fonction pour afficher les images dans la modale avec une icône de poubelle
+function displayImages() {
+  const imagesURL = 'http://localhost:5678/api/works';
+
+  fetch(imagesURL)
+    .then((response) => response.json())
+    .then((data) => {
+      if (Array.isArray(data)) {
+        const categoriesContainer = document.querySelector('.categories');
+        categoriesContainer.innerHTML = ''; // Effacer le contenu actuel avant d'ajouter les nouvelles images
+
+        // Regrouper les images par catégorie
+        const imagesByCategory = {};
+        data.forEach((image) => {
+          if (!imagesByCategory[image.categoryId]) {
+            imagesByCategory[image.categoryId] = [];
+          }
+          imagesByCategory[image.categoryId].push(image);
+        });
+
+        // Afficher les images dans chaque catégorie
+        Object.keys(imagesByCategory).forEach((categoryId) => {
+          const categoryImages = imagesByCategory[categoryId];
+          const categoryDiv = document.createElement('div');
+          categoryDiv.className = 'category';
+
+          categoryImages.forEach((image) => {
+            // Créer un élément de div pour chaque image
+            const imageDiv = document.createElement('div');
+            imageDiv.className = 'image-item';
+
+            // Créer un élément image pour chaque image
+            const imageElement = document.createElement('img');
+            imageElement.src = image.imageUrl;
+
+            // Créer un élément icône de poubelle
+            const deleteIcon = document.createElement('i');
+            deleteIcon.className = 'fas fa-trash delete-icon';
+
+            deleteIcon.addEventListener('click', () => {
+              // Appeler la fonction pour supprimer l'image lorsque l'icône de poubelle est cliquée
+              deleteImage(image.id);
+            });
+
+            // Ajouter l'image et l'icône de poubelle à l'élément d'image
+            imageDiv.appendChild(imageElement);
+            imageDiv.appendChild(deleteIcon);
+
+            // Ajouter l'élément d'image à la div de catégorie
+            categoryDiv.appendChild(imageDiv);
+          });
+
+          // Ajouter la div de catégorie à la div des catégories
+          categoriesContainer.appendChild(categoryDiv);
+        });
+      } else {
+        console.error('Aucune image trouvée ou format de données invalide.');
+      }
+    })
+    .catch((error) => {
+      console.error('Erreur lors de la récupération des images :', error);
+    });
+}
+
+// Fonction pour supprimer une image
+function deleteImage(id) {
+  const deleteURL = `http://localhost:5678/works/${id}`;
+
+  fetch(deleteURL, { method: 'DELETE' })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Image supprimée avec succès:', data);
+      // Mettre à jour les images affichées après suppression
+      displayImages();
+    })
+    .catch((error) => {
+      console.error('Erreur lors de la suppression de l\'image :', error);
+    });
+}
+
+
+
 
 // Fonction pour fermer la modale
 function closeModal() {
@@ -58,6 +140,18 @@ closeModalBtn1.addEventListener('click', () => {
   closeModal();
 });
 
+
+function checkInputs() {
+  const titreValue = inputTitre.value.trim();
+  const categorieValue = selectCategorie.value;
+
+  if (titreValue === '' || categorieValue === '') {
+    alert('Veuillez remplir tous les champs.');
+    btnValider.disabled = true;
+  } else {
+    btnValider.disabled = false;
+  }
+}
   // Événements pour ouvrir et fermer la modale "Ajout photo"
   btnAddPhoto.addEventListener('click', openAddPhotoModal);
   btnCloseAddPhoto.addEventListener('click', closeAddPhotoModal);
@@ -97,4 +191,8 @@ document.addEventListener('DOMContentLoaded', function() {
       previewImage.style.display = 'none'; // Masquer l'image s'il n'y a pas de sélection de fichier
     }
   });
+});
+// Appeler la fonction pour afficher les images au chargement de la page
+document.addEventListener('DOMContentLoaded', function() {
+  displayImages();
 });
