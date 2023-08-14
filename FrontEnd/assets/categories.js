548469815b5d7882e9125c7d68707fe4bd9a6a10
Beugnies Jimmy <93620368+Zrtehhz@users.noreply.github.com>
2023-08-14 22:05:54 +0200

    .

--- FrontEnd/assets/categories.js
+++ FrontEnd/assets/categories.js
@@ -4,13 +4,12 @@
 const items = [];
 const categories = [];
 
-// Fonction pour générer la galerie en fonction du tableau filtré (filteredItems) passé en argument
-const generateGallery = (filteredItems) => {
-  const divGallery = document.querySelector('.gallery');
-  divGallery.innerHTML = '';
+// Fonction pour afficher les items
+const displayItems = (filteredItems) => {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
 
-  // Parcours de chaque élément item dans le tableau filteredItems et création d'éléments figure pour chacun
-  filteredItems.forEach((item) => {
+  filteredItems.forEach(item => {
     const itemElement = document.createElement('figure');
     const imgElement = document.createElement('img');
     imgElement.src = item.imageUrl;
@@ -20,61 +19,52 @@ const generateGallery = (filteredItems) => {
 
     itemElement.appendChild(imgElement);
     itemElement.appendChild(captionElement);
-    divGallery.appendChild(itemElement);
+    gallery.appendChild(itemElement);
   });
 };
 
-// Fonction pour filtrer les éléments items par catégorie en fonction de l'identifiant de catégorie (categoryId)
-const filterItemsByCategory = (categoryId) => {
-  // Si l'identifiant de catégorie est égal à 0, on affiche tous les éléments items, sinon on filtre par identifiant de catégorie
-  const filteredItems = categoryId === 0 ? items : items.filter((item) => item.category.id === categoryId);
-  generateGallery(filteredItems);
+// Filtrage par catégorie
+const filterByCategory = (categoryId) => {
+  const filteredItems = categoryId === 0 ? items : items.filter(item => item.category.id === categoryId);
+  displayItems(filteredItems);
 };
 
-// Attente de chargement du document avant d'exécuter le code
 document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    // Récupération des catégories depuis l'API et ajout de l'option "Tous" au début du tableau
-    const fetchedCategories = await fetchData('http://localhost:5678/api/categories');
-    categories.push({ id: 0, name: 'Tous' }, ...fetchedCategories);
-
-    // Récupération des éléments depuis l'API
-    const fetchedItems = await fetchData('http://localhost:5678/api/works');
-    items.push(...fetchedItems);
-
-    // Création des boutons de filtrage en fonction des catégories disponibles
-    const filterContainer = document.getElementById('filtres');
-    categories.forEach((category) => {
-      const button = document.createElement('button');
-      button.textContent = category.name;
-      button.dataset.categoryId = category.id;
-      button.addEventListener('click', () => {
-        // Au clic sur un bouton de filtre, on filtre les éléments items en fonction de l'identifiant de catégorie associé au bouton
-        filterItemsByCategory(parseInt(button.dataset.categoryId));
-        // On retire la classe 'active' de tous les autres boutons et on l'ajoute au bouton cliqué pour le mettre en surbrillance
-        document.querySelectorAll('#filtres button').forEach((btn) => btn.classList.remove('active'));
-        button.classList.add('active');
-      });
-      
-      filterContainer.appendChild(button);
-    });
-
-    // Génération de la galerie avec tous les éléments items au chargement de la page
-    generateGallery(items);
-  } catch (error) {
-    console.log('Erreur lors de la récupération des données');
-  }
-});
-
-// Fonction pour récupérer des données à partir de l'URL spécifiée
-const fetchData = async (url) => {
-  try {
+  // Récupérer les données depuis l'API
+  const fetchData = async (url) => {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Erreur lors de la récupération des données');
     }
     return await response.json();
+  };
+
+  try {
+    const fetchedCategories = await fetchData('http://localhost:5678/api/categories');
+    categories.push({ id: 0, name: 'Tous' }, ...fetchedCategories);
+
+    const fetchedItems = await fetchData('http://localhost:5678/api/works');
+    items.push(...fetchedItems);
+
+    const filterContainer = document.getElementById('filtres');
+
+    categories.forEach(category => {
+      const button = document.createElement('button');
+      button.textContent = category.name;
+      button.dataset.categoryId = category.id;
+
+      button.addEventListener('click', () => {
+        filterByCategory(parseInt(button.dataset.categoryId));
+        filterContainer.querySelectorAll('button').forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+      });
+
+      filterContainer.appendChild(button);
+    });
+
+    // Afficher tous les items par défaut
+    displayItems(items);
   } catch (error) {
-    console.log(error.message);
+    console.log('Erreur lors de la récupération des données:', error.message);
   }
-};
+});
